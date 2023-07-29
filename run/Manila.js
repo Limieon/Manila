0449adcd6d7afe31a4ec13f2d4adf794c794d991
Limieon <justin.jakob931@outlook.com>
2023-07-29 03:38:49 +0200

    Switched to TypeScript and added parameters to help

--- run/Manila.js
+++ run/Manila.js
@@ -1,24 +1,22 @@
-
 const headless = parameterBoolean('headless', 'Enables compilation flags for headless running')
 const graphicsApi = parameterString('gapi', 'Choose a graphics api', 'opengl')
 const year = parameterNumber('year', 'Enter a year', 2023)
+const test = parameterBoolean('test', 'Run tests')
 
-task('clean')
-	.executes(() => {
-		print('Cleaning...')
-	})
+task('clean').executes(() => {
+	print('Cleaning...')
+})
 
-task('compile')
-	.executes(() => {
-		print('Compiling...')
+task('compile').executes(() => {
+	print('Compiling...')
 
-		if (headless) {
-			print('Compiling Headless...')
-		}
+	if (headless) {
+		print('Compiling Headless...')
+	}
 
-		print(`Compiling for ${graphicsApi}`)
-		print('Year:', year)
-	})
+	print(`Compiling for ${graphicsApi}`)
+	print('Year:', year)
+})
 
 task('run')
 	.dependsOn(':compile')
