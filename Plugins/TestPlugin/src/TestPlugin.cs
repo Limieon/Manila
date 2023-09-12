1dc466f97a4c68659e3b8e76efe8f099f7d582fb
Limieon <justin.jakob931@outlook.com>
2023-09-13 01:15:24 +0200

    Added test plugin sources

---
+++ Plugins/TestPlugin/src/TestPlugin.cs
@@ -0,0 +1,16 @@
+﻿
+using Manila.Plugin.API;
+
+namespace TestPlugin;
+
+public class TestPlugin : Plugin {
+	public static TestPlugin instance { get; private set; }
+
+	public TestPlugin() : base("Test Plugin", "A test plugin", "1.0.0") {
+		instance = this;
+	}
+
+	public override void init() {
+		markup("Initializing [purple]Test Plugin[/]...");
+	}
+}
