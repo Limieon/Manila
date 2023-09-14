ff032f983929a31f4413b25ccf62c83d39b78d06
Limieon <xxgmasterhdxx@gmail.com>
2023-09-14 23:33:50 +0200

    Extended FS API, Added Logger, Added FileUtils

--- Manila/src/Launcher.cs
+++ Manila/src/Launcher.cs
@@ -1,29 +1,46 @@
 ﻿
 using Manila.Commands;
 using Manila.Scripting;
+using Manila.Utils;
+using Manila.CLI.Exceptions;
 
-namespace Manila {
-	class Launcher {
-		public static void Main(string[] args) {
+namespace Manila;
+
+class Launcher {
+	public static void Main(string[] args) {
 #if DEBUG
-			Directory.SetCurrentDirectory("../run/");
+		Directory.SetCurrentDirectory("../run/");
 #endif
 
-			if (args.Length > 0 && args[0].StartsWith(":")) ScriptEngine.getInstance().run().getTask(args[0][1..]).execute();
+		var verbose = false;
+		foreach (var s in args) {
+			if (s == "-v" || s == "--verbose") verbose = true;
+		}
 
-			try {
-				new CLI.App("Manila", "A Build System written in [green4]C#[/] using [yellow]JavaScript[/] as Build Scripts")
-					.setDefaultCommand(new CommandManila())
-					.setHelpCommand(new CommandHelp())
-					.addCommand(new CommandInit())
-					.parse(args);
-			} catch (CLI.Exceptions.ParameterNotProivdedException e) {
-				Console.WriteLine("Missing Parameter '" + e.parameter.name + "' on Command '" + e.command.name + "'!");
-			} catch (CLI.Exceptions.ParameterProvidedWrongTypeException e) {
-				Console.WriteLine("Paremeter '" + e.parameter.name + "' has wrong type (" + e.parameter.type.ToString().ToLower() + " required)!");
-			} catch (CLI.Exceptions.OptionProvidedNotFoundExceptions e) {
-				Console.WriteLine("Could not find option '" + e.option + "' on command '" + e.command.name + "'!");
-			}
+		Logger.init(verbose);
+
+		var rootDir = Scripting.API.Manila.dir(Directory.GetCurrentDirectory());
+		Logger.debug("Root Dir:", rootDir.getPath());
+		FileUtils.init(rootDir);
+		if (args.Length > 0 && args[0].StartsWith(":")) {
+			ScriptEngine.getInstance().run().getTask(args[0][1..]).execute();
+		}
+
+		try {
+			new CLI.App("Manila", "A Build System written in [green4]C#[/] using [yellow]JavaScript[/] as Build Scripts")
+				.setDefaultCommand(new CommandManila())
+				.setHelpCommand(new CommandHelp())
+				.addCommand(new CommandInit())
+				.parse(args);
+		} catch (ParameterNotProivdedException e) {
+			Console.WriteLine("Missing Parameter '" + e.parameter.name + "' on Command '" + e.command.name + "'!");
+			Logger.exception(e);
+		} catch (ParameterProvidedWrongTypeException e) {
+			Console.WriteLine("Paremeter '" + e.parameter.name + "' has wrong type (" + e.parameter.type.ToString().ToLower() + " required)!");
+			Logger.exception(e);
+		} catch (OptionProvidedNotFoundExceptions e) {
+			Console.WriteLine("Could not find option '" + e.option + "' on command '" + e.command.name + "'!");
+			Logger.exception(e);
 		}
 	}
 }
