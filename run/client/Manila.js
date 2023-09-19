09afdc274393bbf57e59497568535dc074176baf
Limieon <xxgmasterhdxx@gmail.com>
2023-09-19 22:58:12 +0200

    Updated script files

--- run/client/Manila.js
+++ run/client/Manila.js
@@ -1,31 +1,35 @@
 const project = Manila.getProject()
 const workspace = Manila.getWorkspace()
 
+let binary = undefined
+
 task('compile').onExecute(() => {
-	print('Compiling Client...')
+	Manila.print('Compiling Client...')
 
 	const config = Manila.getConfig()
-	print('Platform:', config.platform)
-	print('Config:', config.config)
-	print('Arch:', config.arch)
+	Manila.print('Platform:', config.platform)
+	Manila.print('Config:', config.config)
+	Manila.print('Arch:', config.arch)
 
 	const flags = ManilaCPP.clangFlags()
 	flags.name = project.name
-	flags.binDir = Manila.dir(project.location).join('bin').join(config.platform).join(`${config.name}-${config.arch}`).join(flags.name)
-	flags.objDir = Manila.dir(project.location).join('bin-int').join(config.platform).join(`${config.name}-${config.arch}`).join(flags.name)
+	flags.binDir = Manila.dir(project.location).join('bin').join(config.platform).join(`${config.config}-${config.arch}`).join(flags.name)
+	flags.objDir = Manila.dir(project.location)
+		.join('bin-int')
+		.join(config.platform)
+		.join(`${config.config}-${config.arch}`)
+		.join(flags.name)
 
 	flags.files = Manila.dir(project.location)
 		.join('src')
 		.files(f => f.endsWith('.cpp') || f.endsWith('.c'), true)
 
-	print(flags.files)
-
 	const res = ManilaCPP.clangCompile(flags, project, workspace)
+	binary = res.binary
 })
 
 task('run')
 	.dependsOn(':client:compile')
-	.dependsOn(':tests:client:run')
 	.onExecute(() => {
-		print('Running...')
+		Manila.print('Executing', binary.getPath())
 	})
